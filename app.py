76d55355434b7c3103dd0e2f527b792d268adef6
George Ciesinski <george.ciesin@gmail.com>
2019-11-17 21:09:49 -0500

    Started building UI

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ import cv2
 import numpy
 
 
+# Dummy function that does nothing (as a dummy event handler for Trackbars)
+def dummy():
+    pass
+
+
 # Create the UI (Window and trackbars)
-cv2.namedWindow('Image Filters')
+cv2.namedWindow('Image Filters', cv2.WINDOW_GUI_NORMAL)
+# TODO: Get rid of the black rectangle in window
+
+# TODO: Make trackbars the same width in Linux...
+# Arguments: trackbarName, windowName, value (initial), count (max value), onChange (event handler)
+# Contrast Trackbar
+cv2.createTrackbar('contrast', 'Image Filters', 1, 100, dummy)
+# Brightness Trackbar - initial value is 50 to compensate for negative brightness (cv doesn't allow negative values)
+cv2.createTrackbar('brightness', 'Image Filters', 50, 100, dummy)
+# Filter Trackbar
+# TODO: Update max value to number of filters
+cv2.createTrackbar('filters', 'Image Filters', 0, 1, dummy)
+# Grayscale Trackbar - switch only: values 0 & 1.
+cv2.createTrackbar('grayscale', 'Image Filters', 0, 1, dummy)
+
 
 # TODO: Remove this line!
 cv2.waitKey(0)
