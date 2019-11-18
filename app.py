312c48029857e19c273347e8f4fe6bac15d810c8
George Ciesinski <george.ciesin@gmail.com>
2019-11-17 20:09:43 -0500

    Initial commit.

---
+++ app.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy
+
+
+# Create the UI (Window and trackbars)
+cv2.namedWindow('Image Filters')
+
+# TODO: Remove this line!
+cv2.waitKey(0)
+# Window Cleanup
+cv2.destroyAllWindows()
