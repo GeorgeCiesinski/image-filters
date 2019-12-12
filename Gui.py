69b9245de725441091ed4e01e0f92f52b3142628
George Ciesinski <george.ciesin@gmail.com>
2019-12-11 20:07:44 -0500

    Moved Kernels into a new module, confirmed working.

---
+++ Gui.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 20:06:11 2019
+
+@author: georg
+"""
+
--- app.py
+++ app.py
@@ -7,65 +7,16 @@ Created on Mon Dec  2 20:37:51 2019
 
 import os
 import cv2
-import numpy as np
 import tkinter
+import logging
+import webbrowser
+import numpy as np
 from tkinter import filedialog
 from tkinter import messagebox
 from PIL import Image
 from PIL import ImageTk
-import logging
-import webbrowser
+from Kernels import Kernels
 
-
-class Kernels:
-    
-    """
-    Define convolution kernels
-    """
-    
-    # Kernels
-    
-    identity_kernel = np.array([
-            [0, 0, 0],
-            [0, 1, 0], 
-            [0, 0, 0]
-            ])
-        
-    sharpen_kernel = np.array([
-            [0, -1, 0],
-            [-1, 5, -1],
-            [0, -1, 0]
-            ])
-    
-    # getGaussianKernel(size, standard_deviation) | Larger numbers result in more blurring
-    gaussian_kernel1 = cv2.getGaussianKernel(3, 0)
-    
-    gaussian_kernel2 = cv2.getGaussianKernel(5, 0)
-    
-    # Averaging kernel (takes average of pixel values in the window)
-    box_kernel = np.array([
-            [1, 1, 1], 
-            [1, 1, 1], 
-            [1, 1, 1]
-            ], np.float32) / 9
-        
-    # Kernel array
-    k_array = [
-            identity_kernel, 
-            sharpen_kernel, 
-            gaussian_kernel1, 
-            gaussian_kernel2, 
-            box_kernel
-            ]
-    
-    # Kernel names (mirrors above array)
-    k_name = [
-            "Identity Kernel",
-            "Sharpen Kernel",
-            "Gaussian Kernel 1",
-            "Gaussian Kernel 2",
-            "Box Kernel",
-            ]
 
 class Gui:
     
